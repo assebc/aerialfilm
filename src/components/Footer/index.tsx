0a497077734ede1685ec255faaaa4b8d5abd7bad
José Rodrigues <jose15.luis00@gmail.com>
2023-08-28 22:47:20 +0100

    Refactor: responsive footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,5 +1,5 @@
 import { FC } from "react";
-import { Layout, Avatar, Space } from "antd";
+import { Layout, Avatar, Row, Col } from "antd";
 import { InstagramOutlined, MailOutlined } from "@ant-design/icons";
 import "./styles.css";
 
@@ -7,44 +7,43 @@ export const Footer: FC = () => {
 
     return (
         <Layout.Footer className="footer">
-            <div className="socials">
+            <Row gutter={[16, 16]} className="socials" wrap>
+                <Col md={8} sm={24} xs={24}>
+                    <div className="left_socials">
+                        CREWS & RESOURCES IN THE KEY FILM <br/>
+                        PRODUCTION MARKETS OF PORTUGAL
+                    </div>
+                </Col>
 
-                <div>
-                    CREWS & RESOURCES IN THE KEY FILM <br/>
-                    PRODUCTION MARKETS OF PORTUGAL
-                </div>
-
-                <div className="center_socials">
-                    <Space wrap size={16}>
-                        <a
-                            href="https://www.instagram.com/aerialfilm.23/"
-                            target="_blank"
-                        >
-                            <Avatar className="socials_avatar" size={56}>
-                                <InstagramOutlined />
-                            </Avatar>
-                        </a>
-                    
-                        <a
-                            href="mailto: aerialfilm.23@gmail.com"
-                            target="_blank"
-                            >
-                            <Avatar className="socials_avatar" size={56}> 
-                                <MailOutlined />
-                            </Avatar> 
-                        </a>
-                    </Space>
-                </div>
-
-                <div className="right_socials">
-                    AERIAL CINEMATOGRAPHY & DRONE <br />
-                    PILOTING SERVICES <br />
-                </div>
+                <Col md={8} sm={24} xs={24} className="center_socials">
+                    <a
+                        href="https://www.instagram.com/aerialfilm.23/"
+                        target="_blank"
+                    >
+                        <Avatar className="socials_avatar" size={56}>
+                            <InstagramOutlined />
+                        </Avatar>
+                    </a>
                 
-            </div>
+                    <a
+                        href="mailto: aerialfilm.23@gmail.com"
+                        target="_blank"
+                        >
+                        <Avatar className="socials_avatar" size={56}> 
+                            <MailOutlined />
+                        </Avatar> 
+                    </a>
+                </Col>
+
+                <Col md={8} sm={24} xs={24}>
+                    <div className="right_socials">
+                        AERIAL CINEMATOGRAPHY & DRONE <br />
+                        PILOTING SERVICES <br />
+                    </div>
+                </Col>
+            </Row>
             
             <div className="credits">
-                
                 AerialFilm {new Date().getFullYear()} Created by {" "} 
                 <a
                     href="https://bitsway.dev" 

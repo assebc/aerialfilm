02febbafda86c45ab2da2e1594393a50dc0618ad
galleonpt <jose15.luis00@gmail.com>
2023-08-31 17:25:11 +0100

    refactor: work page responsiveness

--- src/pages/Work/index.tsx
+++ src/pages/Work/index.tsx
@@ -1,56 +1,47 @@
 import { FC } from "react";
 import "./styles.css";
-import video1 from "../../assets/videos/video4.mp4";
-import video2 from "../../assets/videos/video2_2.mp4";
-import video3 from "../../assets/videos/video2_3.mp4";
-import video4 from "../../assets/videos/video2_4.mp4";
-import { Space } from "antd";
+import { Col, Row } from "antd";
 
-export const Work : FC = ({}) => {
+export const Work : FC = () => {
     return (
         <div className="body">
             <div className="single_header">
                 <h1>VIEW OUR VARIOUS REELS OVER THE LAST SEVERAL MONTHS.</h1>
             </div>
 
-            <div className="navigate_avatars">
-                <div className="avatars">
+            <Row gutter={[0, 50]} justify={"center"} align={"middle"}>
+                <Row gutter={[16, 16]}>
+                    <Col md={12} sm={24} xs={24} className="desc_video">
+                        <iframe src="https://www.youtube.com/embed/WBs3G1PvyfM?si=j8ZiLhkGolXTyITW&autoplay=1&mute=1" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" />
 
-                    <Space direction="horizontal" size={240}>
-                        <Space direction="vertical" size={100}>
-                            <div className="desc_video">
-                                <video width={400} src={video1} autoPlay={false} controls controlsList="nodownload"/>
-                                <h6>2022 Landscape Flow Reel</h6>
-                                <span>AERIALFILM</span>
-                            </div>
-                            
-                            <div className="desc_video">
-                                <video width={400} src={video4} autoPlay={false} controls controlsList="nodownload"/>
-                                <h6>2023 Trail Erdal Reel</h6>
-                                <span>AERIALFILM</span>
-                            </div>
-                        </Space>
+                        <h6>2022 Landscape Flow Reel</h6>
+                        <span>AERIALFILM</span>
+                    </Col>
+                    
+                    <Col md={12} sm={24} xs={24} className="desc_video">
+                        <iframe src="https://www.youtube.com/embed/WBs3G1PvyfM?si=j8ZiLhkGolXTyITW&autoplay=1&mute=1" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" />
 
-                        <Space direction="vertical" size={100}>
-                            
-                            
-                            <div className="desc_video">
-                                <video width={400} src={video2} autoPlay={false} controls controlsList="nodownload"/>
-                                <h6>2023 MotoFestival Reel</h6>
-                                <span>AERIALFILM</span>
-                            </div>
+                        <h6>2023 Trail Erdal Reel</h6>
+                        <span>AERIALFILM</span>
+                    </Col>
+                </Row>
 
-                            <div className="desc_video">
-                                <video width={400} src={video3} autoPlay={false} controls controlsList="nodownload"/>
-                                <h6>2023 9th Drift Meet Reel</h6>
-                                <span>AERIALFILM</span>
-                            </div> 
-                        </Space>
-                        
-                    </Space>
+                <Row gutter={[16, 16]}>
+                    <Col md={12} sm={24} xs={24} className="desc_video">
+                        <iframe src="https://www.youtube.com/embed/WBs3G1PvyfM?si=j8ZiLhkGolXTyITW&autoplay=1&mute=1" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" />
 
-                </div>
-            </div>
+                        <h6>2023 MotoFestival Reel</h6>
+                        <span>AERIALFILM</span>
+                    </Col>
+
+                    <Col md={12} sm={24} xs={24} className="desc_video">
+                        <iframe src="https://www.youtube.com/embed/WBs3G1PvyfM?si=j8ZiLhkGolXTyITW&autoplay=1&mute=1" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" />
+
+                        <h6>2023 9th Drift Meet Reel</h6>
+                        <span>AERIALFILM</span>
+                    </Col> 
+                </Row> 
+            </Row>
 
         </div>
     );

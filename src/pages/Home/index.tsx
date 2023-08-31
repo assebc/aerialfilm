da13dc2e34c635ef43b4f8b025215db9fe62e3e9
galleonpt <jose15.luis00@gmail.com>
2023-08-31 18:26:19 +0100

    refactor: home page responsiveness

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,13 +1,12 @@
 import { FC } from "react";
 import { useNavigate } from "react-router-dom";
-import { Avatar, Button, Space } from "antd";
+import { Avatar, Button, Col, Row } from "antd";
 import {
   AuditOutlined,
   ContactsOutlined,
   TeamOutlined,
   VideoCameraOutlined,
 } from "@ant-design/icons";
-import video1 from "../../assets/videos/video1.mp4";
 import { handleNavigate } from "../../utils/handleNavigate";
 import { ROUTES } from "../../configs/routes";
 import "./styles.css";
@@ -21,12 +20,12 @@ export const Home: FC = () => {
         <div className="over_text_home">
           <h1>VIEW OUR WORK</h1>
           <h3>FEATURING OUR WORK IN ACTION SPORTS</h3>
-          <Button onClick={() => handleNavigate(navigate, ROUTES.WORK)}>
+          <Button className="reels_btn" onClick={() => handleNavigate(navigate, ROUTES.WORK)}>
             VIEW REELS
           </Button>
         </div>
 
-        <video src={video1} loop autoPlay={true} muted />
+        <iframe src="https://www.youtube.com/embed/WBs3G1PvyfM?si=j8ZiLhkGolXTyITW&autoplay=1&mute=1" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" />
       </div>
 
       <div className="bio_wrapper">
@@ -40,115 +39,112 @@ export const Home: FC = () => {
           </p>
         </div>
 
-        <div className="navigate_avatars">
-          <div className="avatars">
-            <Space direction="horizontal" size={240}>
-              <Space direction="vertical" size={100}>
-                <div className="desc">
-                  <Avatar
-                    size={"large"}
-                    style={{
-                      width: "90px",
-                      height: "90px",
-                      backgroundColor: "#DB0101",
-                      display: "flex",
-                      justifyContent: "center",
-                      alignItems: "center",
-                    }}
-                    icon={<AuditOutlined />}
-                  />
-                  <h1>TOP DRONE CREWS</h1>
-                  <p>
-                    Headed by professional aerial
-                    <br /> cinematographers, our production teams deliver
-                    <br /> on the directors vision.
-                  </p>
-                  <Button onClick={() => handleNavigate(navigate, ROUTES.HOME)}>
-                    MEET OUR TEAM
-                  </Button>
-                </div>
+        <div className="navigation_section">
 
-                <div className="desc">
-                  <Avatar
-                    size={"large"}
-                    style={{
-                      width: "90px",
-                      height: "90px",
-                      backgroundColor: "#DB0101",
-                      display: "flex",
-                      justifyContent: "center",
-                      alignItems: "center",
-                    }}
-                    icon={<ContactsOutlined />}
-                  />
-                  <h1>STRATEGIC PARTNERS</h1>
-                  <p>
-                    We have long standing frindships and strategic
-                    <br /> partnerships with several other companies
-                    <br /> including other drone/aerial companies.
-                  </p>
-                  <Button
-                    onClick={() => handleNavigate(navigate, ROUTES.ABOUT_US)}
-                  >
-                    ABOUT US
-                  </Button>
-                </div>
-              </Space>
+        <Row gutter={[16, 16]} >
+          <Col  md={12} sm={24} xs={24} className="desc">
+            <Avatar
+              size={"large"}
+              style={{
+                width: "90px",
+                height: "90px",
+                backgroundColor: "#DB0101",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+              icon={<AuditOutlined />}
+            />
+            <h1>TOP DRONE CREWS</h1>
+            <p>
+              Headed by professional aerial
+              <br /> cinematographers, our production teams deliver
+              <br /> on the directors vision.
+            </p>
+            <Button onClick={() => handleNavigate(navigate, ROUTES.HOME)}>
+              MEET OUR TEAM
+            </Button>
+          </Col>
 
-              <Space direction="vertical" size={100}>
-                <div className="desc">
-                  <Avatar
-                    size={"large"}
-                    style={{
-                      width: "90px",
-                      height: "90px",
-                      backgroundColor: "#DB0101",
-                      display: "flex",
-                      justifyContent: "center",
-                      alignItems: "center",
-                    }}
-                    icon={<VideoCameraOutlined />}
-                  />
-                  <h1>CAMERA SYSTEMS</h1>
-                  <p>
-                    We own an array of cinema/drone camera
-                    <br /> systems, lensing, and stabilizers that cover a
-                    <br /> variety of cinematography needs.
-                  </p>
-                  <Button onClick={() => handleNavigate(navigate, ROUTES.GEAR)}>
-                    OUR GEAR
-                  </Button>
-                </div>
+          <Col  md={12} sm={24} xs={24} className="desc">
+            <Avatar
+              size={"large"}
+              style={{
+                width: "90px",
+                height: "90px",
+                backgroundColor: "#DB0101",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+              icon={<ContactsOutlined />}
+            />
+            <h1>STRATEGIC PARTNERS</h1>
+            <p>
+              We have long standing frindships and strategic
+              <br /> partnerships with several other companies
+              <br /> including other drone/aerial companies.
+            </p>
+            <Button
+              onClick={() => handleNavigate(navigate, ROUTES.ABOUT_US)}
+            >
+              ABOUT US
+            </Button>
+          </Col>
+        </Row>
 
-                <div className="desc">
-                  <Avatar
-                    size={"large"}
-                    style={{
-                      width: "90px",
-                      height: "90px",
-                      backgroundColor: "#DB0101",
-                      display: "flex",
-                      justifyContent: "center",
-                      alignItems: "center",
-                    }}
-                    icon={<TeamOutlined />}
-                  />
-                  <h1>COLLABORATION</h1>
-                  <p>
-                    Our team of industry professionals has vast
-                    <br /> experience coordinating with studio cews form
-                    <br /> pre-through post-production.
-                  </p>
-                  <Button
-                    onClick={() => handleNavigate(navigate, ROUTES.CONTACT_US)}
-                  >
-                    CONTACT US
-                  </Button>
-                </div>
-              </Space>
-            </Space>
-          </div>
-        </div>
+        <Row gutter={[16, 16]}>
+          <Col  md={12} sm={24} xs={24} className="desc">
+            <Avatar
+              size={"large"}
+              style={{
+                width: "90px",
+                height: "90px",
+                backgroundColor: "#DB0101",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+              icon={<VideoCameraOutlined />}
+            />
+            <h1>CAMERA SYSTEMS</h1>
+            <p>
+              We own an array of cinema/drone camera
+              <br /> systems, lensing, and stabilizers that cover a
+              <br /> variety of cinematography needs.
+            </p>
+            <Button onClick={() => handleNavigate(navigate, ROUTES.GEAR)}>
+              OUR GEAR
+            </Button>
+          </Col>
+
+          <Col  md={12} sm={24} xs={24} className="desc">
+            <Avatar
+              size={"large"}
+              style={{
+                width: "90px",
+                height: "90px",
+                backgroundColor: "#DB0101",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+              icon={<TeamOutlined />}
+            />
+            <h1>COLLABORATION</h1>
+            <p>
+              Our team of industry professionals has vast
+              <br /> experience coordinating with studio cews form
+              <br /> pre-through post-production.
+            </p>
+            <Button
+              onClick={() => handleNavigate(navigate, ROUTES.CONTACT_US)}
+            >
+              CONTACT US
+            </Button>
+          </Col>
+        </Row>
+      </div>
       </div>
     </>
   );

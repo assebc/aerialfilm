6780c4bb3b00ad7dd9799e89efd01548c2027fd8
galleonpt <jose15.luis00@gmail.com>
2023-08-31 23:22:33 +0100

    fix wrong styles

--- src/pages/Work/index.tsx
+++ src/pages/Work/index.tsx
@@ -9,36 +9,43 @@ export const Work : FC = () => {
                 <h1>VIEW OUR VARIOUS REELS OVER THE LAST SEVERAL MONTHS.</h1>
             </div>
 
-            <Row gutter={[0, 50]} justify={"center"} align={"middle"}>
-                <Row gutter={[16, 16]}>
+            <Row gutter={[0, 50]}>
+                <Row gutter={[16, 16]} style={{width:"100%"}}>
                     <Col md={12} sm={24} xs={24} className="desc_video">
-                        <iframe src="https://www.youtube.com/embed/WBs3G1PvyfM?si=j8ZiLhkGolXTyITW&autoplay=1&mute=1" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" />
-
-                        <h6>2022 Landscape Flow Reel</h6>
-                        <span>AERIALFILM</span>
+                        <div>
+                            <iframe src="https://www.youtube.com/embed/WBs3G1PvyfM?si=j8ZiLhkGolXTyITW&autoplay=1&mute=1" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" />
+                            <h6>2022 Landscape Flow Reel</h6>
+                            <span>AERIALFILM</span>
+                        </div>
                     </Col>
                     
                     <Col md={12} sm={24} xs={24} className="desc_video">
-                        <iframe src="https://www.youtube.com/embed/WBs3G1PvyfM?si=j8ZiLhkGolXTyITW&autoplay=1&mute=1" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" />
+                        <div>
+                            <iframe src="https://www.youtube.com/embed/WBs3G1PvyfM?si=j8ZiLhkGolXTyITW&autoplay=1&mute=1" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" />
 
-                        <h6>2023 Trail Erdal Reel</h6>
-                        <span>AERIALFILM</span>
+                            <h6>2023 Trail Erdal Reel</h6>
+                            <span>AERIALFILM</span>
+                        </div>
                     </Col>
                 </Row>
 
-                <Row gutter={[16, 16]}>
+                <Row gutter={[16, 16]} justify={"center"} align={"middle"} style={{width:"100%"}}>
                     <Col md={12} sm={24} xs={24} className="desc_video">
-                        <iframe src="https://www.youtube.com/embed/WBs3G1PvyfM?si=j8ZiLhkGolXTyITW&autoplay=1&mute=1" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" />
+                        <div>
+                            <iframe src="https://www.youtube.com/embed/WBs3G1PvyfM?si=j8ZiLhkGolXTyITW&autoplay=1&mute=1" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" />
 
-                        <h6>2023 MotoFestival Reel</h6>
-                        <span>AERIALFILM</span>
+                            <h6>2023 MotoFestival Reel</h6>
+                            <span>AERIALFILM</span>
+                        </div>
                     </Col>
 
                     <Col md={12} sm={24} xs={24} className="desc_video">
-                        <iframe src="https://www.youtube.com/embed/WBs3G1PvyfM?si=j8ZiLhkGolXTyITW&autoplay=1&mute=1" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" />
+                        <div>
+                            <iframe src="https://www.youtube.com/embed/WBs3G1PvyfM?si=j8ZiLhkGolXTyITW&autoplay=1&mute=1" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" />
 
-                        <h6>2023 9th Drift Meet Reel</h6>
-                        <span>AERIALFILM</span>
+                            <h6>2023 9th Drift Meet Reel</h6>
+                            <span>AERIALFILM</span>
+                        </div>
                     </Col> 
                 </Row> 
             </Row>
